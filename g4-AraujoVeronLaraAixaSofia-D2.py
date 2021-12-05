d961d67f572cdfa67cdf8ab953b2fa1e31ab8593
aixa-sofia <sofiaarauko@gmail.com>
2021-12-05 07:42:21 -0300

    Primera versión

---
+++ g4-AraujoVeronLaraAixaSofia-D2.py
@@ -0,0 +1,9 @@
+
+agenda = {input("Nombre: "): input("Telefóno: "), input("Nombre: "): input("Telefóno: "), input("Nombre: "): input("Telefóno: "), input("Nombre: "): input("Telefóno: "), input("Nombre: "): input("Telefóno: "), input("Nombre: "): input("Telefóno: ")}
+print (agenda)
+
+print(agenda[input("Nombre: ")])
+
+
+
+
